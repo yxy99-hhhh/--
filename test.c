3140965df1c8ca39ed4fefbaf0afa3a52f823ed0
yxy99-hhhh <116446886+yxy99-hhhh@users.noreply.github.com>
2022-11-02 19:33:55 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+//int main() {
+//	int n = 0;
+//	int ret = 1;
+//	scanf_s("%d",&n);
+//	for (int i = 1; i <= n; i++) {
+//
+//		ret *= i;
+//
+//
+//
+//
+//	}
+//	printf("%d",ret);
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+int main() {
+
+	int n = 0;
+	scanf_s("%d",&n);
+	
+	int ret=fac();
+	printf("%d",ret);
+
+
+
+
+
+
+	return 0;
+}
